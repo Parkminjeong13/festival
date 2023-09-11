f30f7609fa82c0ed5c1704a66dbbb6da3d1ab9cd
박민정 <ming1320@gmail.com>
2023-09-11 09:40:33 +0900

    230911 수업시작

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -78,19 +78,17 @@ const Pagenation = styled.div`
     align-items: center;
     li{
       border: 1px solid #ddd;
+      padding: 5px 20px;
       border-radius: 5px; cursor: pointer;
       background-color: #fff;
       &.on{
         background-color: violet;
         font-weight: bold;
-      }
-      &.on a{
         color: #fff;
       }
       a{
         display: inline-block;
         width: 100%;
-        padding: 5px 20px;
       }
     }
   }
@@ -99,7 +97,7 @@ const Pagenation = styled.div`
 function Main() {
 
   const [data, setData] = useState();
-
+  const [allData, setAllData] = useState();
   const list = 10;
   const [page, setPage] = useState(1);
   const [totalCnt, setTotalCnt] = useState(0);
@@ -107,14 +105,42 @@ function Main() {
   const pagination = 5;
   const totalPage = Math.floor(totalCnt / list);
 
+  let startPage;
+  let endPage;
+
+  const currentBlock = Math.ceil(page / pagination);
+  startPage = (currentBlock - 1) * pagination + 1;
+  endPage = startPage + pagination - 1;
+
+  if(endPage > totalPage){
+    endPage = totalPage;
+  }
+  const PrevBlock = () =>{
+    if(startPage > 1){
+      setPage(startPage - pagination);
+    }
+  }
+  const NextBlock = () =>{
+    if(endPage < totalPage){
+      setPage(startPage + pagination);
+    }
+  }
+
   const PageList = [];
-  for(let i = 0; i < totalPage; i++){
+  for(let i = startPage; i <= endPage; i++){
     PageList.push(
-      <li key={i} className={(page === i+1 ? "on" : "")}>
-        <NavLink to='/'  onClick={()=>{setPage(i+1)}}>{i+1}</NavLink>
+      <li key={i} className={(page === i ? "on" : "")} onClick={()=>{setPage(i)}}>
+        {i}
       </li>
     )
   }
+
+  useEffect(()=>{
+    axios.get(`https://apis.data.go.kr/6260000/FestivalService/getFestivalKr?serviceKey=${process.env.REACT_APP_APIKEY}&pageNo=1&numOfRows=100&resultType=json`)
+    .then(function(res){
+      setAllData(res.data.getFestivalKr.item);
+    })
+  },[])
 
   useEffect(()=>{
     axios.get(`https://apis.data.go.kr/6260000/FestivalService/getFestivalKr?serviceKey=${process.env.REACT_APP_APIKEY}&pageNo=${page}&numOfRows=10&resultType=json`)
@@ -129,8 +155,7 @@ function Main() {
     return gugun === "전체" || gugun === e.GUGUN_NM
   })
 
-  const FilterGugun = data && [...new Set(data.map(e=>e.GUGUN_NM))];
-  console.log(FilterGugun)
+  const FilterGugun = [...new Set(allData && allData.map(e=>e.GUGUN_NM))];
 
   const [isActive, setIsActive] = useState(-1);
 
@@ -195,17 +220,9 @@ function Main() {
       </Content>
       <Pagenation>
         <ul>
-          <li onClick={()=>{
-            (page === 1 ? alert("더 이상 데이터가 없습니다.") : setPage(page-1))
-          }}><NavLink to="/">이전</NavLink></li>
-          {
-            data && PageList.map(e=>{
-              return e
-            })
-          }
-          <li onClick={()=>{
-            (page > totalPage ? alert("더 이상 데이터가 없습니다.") : setPage(page+1))
-          }}><NavLink to="/">다음</NavLink></li>
+          <li onClick={PrevBlock}>이전</li>
+          {PageList}
+          <li onClick={NextBlock}>다음</li>
         </ul>
       </Pagenation>
     </>
